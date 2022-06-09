bca67af31f6fcb90892d1509c687616ed5768dd5
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-06-09 14:33:11 +0530

    Collections

---
+++ test.js
@@ -0,0 +1,60 @@
+
+
+
+// // let a =[1,2,3,4,5,5,7,8,3,2,34,1,35,6];
+// // let arr = a.sort((a, b)=>a-b)
+// // // console.log(arr);
+
+// // let out = [];
+
+// // for(let i = 0; i < arr.length; i++){
+// //     if(arr[i] !== arr[i+1]){
+// //         out.push(arr[i]);
+// //     }
+// // }
+
+// // console.log(out);
+
+
+
+// package com.masai.java;
+
+// public class MultiplicationTable {
+// 		void print() {
+// 			print(5);
+// 		}
+// 	     void print(int k){
+// 	         print(k,1,10);
+// 	     }
+// 	    void print(int k, int from, int to){
+// 	        for(int i=from; i<=to; i++){
+// 	            System.out.printf("%d * %d = %d", k,i,i*k).println();
+// 	        }
+// 	    }
+// //	    public static void main(String[] args){
+// //	        MultiplicationTable table = new MultiplicationTable();
+// //	        // table.print();
+// //	        table.print(10, 1, 10);
+// //	    }
+	
+// }
+
+
+// package com.masai.java;
+
+// public class MultiplicationRunner {
+
+// 	public static void main(String[] args) {
+// 		// TODO Auto-generated method stub
+// 		MultiplicationTable table = new MultiplicationTable();
+// 		table.print();
+//         table.print(11);
+//         table.print(10, 1, 10);
+// 	}
+
+// }
+
+
+
+
+
