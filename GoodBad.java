54b48b8960ca9396ced795e3a83a3fe235e95ab6
ROHIT KUMAR <rohit9909mr@gmail.com>
2022-07-25 12:58:58 +0530

    Good Bad

--- GoodBad.java
+++ GoodBad.java
@@ -4,22 +4,22 @@ class GoodBad{
     public static void goodAndBad(String str, String good)
     {
         str+=" ";
-        for(int i=0; i < str.length()-1; i++){
-            if(str.charAt(i)!=str.charAt(i+1)){
-                good+=str.charAt(i);
+            for(int i=0; i < str.length()-1; i++){
+                if(str.charAt(i)!=str.charAt(i+1)){
+                    good+=str.charAt(i);
+                }
             }
-        }
-        System.out.println(good);
+            System.out.println(good);
     }
     
     
     public static void main(String[] args){
-            // Scanner sc = new Scanner(System.in);
-            // int t = sc.nextInt();
-            // while(t--!=0){
-            //     String str = sc.next();
-            //     goodAndBad(str, "");
-            // }
+        // Scanner sc = new Scanner(System.in);
+        // int t = sc.nextInt();
+        // while(t--!=0){
+        //     String str = sc.next();
+        //     goodAndBad(str, "");
+        // }
         
         String str = "aaab";
         goodAndBad(str, "");
